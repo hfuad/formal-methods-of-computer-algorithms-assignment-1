88e22a9a1fe6359e524cdd37bea668369e852169
hfuad <hfuad@sumerge.com>
2018-04-07 15:34:16 +0200

    MOD : All questions + comparison + Readme update

--- Assignment1.py
+++ Assignment1.py
@@ -1,20 +1,17 @@
 import math
 import random
+import time
 
-# Formal Methods of Computer Algorithms - Assignment 1
-# Riham Nour
-# Hussam Fuad 161101
+print("# Formal Methods of Computer Algorithms - Assignment 1")
+print("\tRiham Nour  182031")
+print("\tHussam Fuad 161101")
 
 
 # prepare random points
-vList = [
-        (20, 20),
-        (30, 50),
-        (60, 70),
-        (20, 30),
-        (90, 5)
-        ]
-print(vList)
+vlist = [(20, 20), (30, 50), (60, 70), (20, 30), (90, 5)]
+
+print("## Sample points")
+print(vlist)
 
 
 # distance calc
@@ -22,71 +19,95 @@ def euclidean_distance(p1, p2):
     return math.sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
 
 
+def display(vlist, minimum_index, minimum_Distance, minimize_max_distance_index, minimize_max_distance, iterations):
+    print("### display for n=",len(vlist))
+    print("#### 1) Using brute-force approach, design an algorithm to find the post-office location "
+          "minimizing the average distance between the villages and the post office.")
+    print("\tmin distance:", minimum_index, "\tvalue:", minimum_Distance)
+    print("")
+    print("#### 2) Using brute-force approach, design an algorithm to find the post-office location "
+          "minimizing the maximum distance from a village to the post office.")
+    print("\tminimize max:", minimize_max_distance_index, "\tvalue:", minimize_max_distance)
+    print("")
+    print("#### 4) Analyze your designed algorithms mathematically and empirically.")
+    print("\tEmpirical Analysis : complexity O(log n**2)")
+    print("\tnumerical Analysis : ")
+    print("\t\tn=", len(vlist))
+    print("\t\titerations=", iterations)
+    print("\t\tcalculation n**2=", (len(vlist))**2)
+    print("\t\tactual  n*(n-1)=", len(vlist)*(len(vlist)-1))
+    print("")
+    print("-------------------")
+
+
 # brute force
-def brute_force(vlist, enableLogging):
+def brute_force(vlist, enableLogging, minAverage, minimizeMax):
     minimum_Distance = 999
     minimum_index = -1
     iterations = 0
     minimize_max_distance = 999
     minimize_max_distance_index = 999
-    for i in range(0, len(vList)):
+
+    for i in range(0, len(vlist)):
         summation = avg = count = 0
         local_maximum = -999
-        for y in range(0, len(vList)):
+        for y in range(0, len(vlist)):
             if i != y:
-                dist = euclidean_distance(vList[i], vList[y])
-                count += 1
-                summation += dist
                 iterations += 1
-                if dist > local_maximum:
-                    local_maximum = dist
+                dist = euclidean_distance(vlist[i], vlist[y])
+                if minAverage:
+                    count += 1
+                    summation += dist
+                if minimizeMax:
+                    if dist > local_maximum:
+                        local_maximum = dist
                 if enableLogging:
                     print("\t", i, ":", y, "\t", dist)
-        avg = summation/count
-        if enableLogging:
-            print("avg:", i, ":", avg)
-            print("")
-        if avg < minimum_Distance:
-            minimum_Distance = avg
-            minimum_index = i
-        if local_maximum<minimize_max_distance:
-            minimize_max_distance = local_maximum
-            minimize_max_distance_index = i
-    return minimum_index,  minimum_Distance, minimize_max_distance_index, minimize_max_distance , iterations
+        if minAverage:
+            avg = summation/count
+            if avg < minimum_Distance:
+                minimum_Distance = avg
+                minimum_index = i
+            if enableLogging:
+                print("avg:", i, ":", avg)
+        if minimizeMax:
+            if local_maximum<minimize_max_distance:
+                minimize_max_distance = local_maximum
+                minimize_max_distance_index = i
+    return minimum_index,  minimum_Distance, minimize_max_distance_index, minimize_max_distance, iterations
 
-minimum_index,  minimum_Distance, minimize_max_distance_index, minimize_max_distance, iterations = brute_force(vList, True)
+minimum_index,  minimum_Distance, minimize_max_distance_index, minimize_max_distance, iterations = brute_force(vlist, False, True, True)
 
-print("-------------------")
-print("min distance:", minimum_index, "\tvalue:", minimum_Distance)
-print("minimize max distance:", minimize_max_distance_index, "\tvalue:", minimize_max_distance)
-print("-------------------")
-print("complexity O(log n**2)")
-print("numerical complexity:")
-print("n=", len(vList))
-print("iterations=", iterations)
-print("calculation n**2=", (len(vList))**2)
-print("actual  n*(n-1)=", len(vList)*(len(vList)-1))
-
-print("######################")
-print("#rerun for 100 random points")
-print("######################")
+display(vlist, minimum_index,  minimum_Distance, minimize_max_distance_index, minimize_max_distance, iterations)
 
 # Generate 1000 random points
-vList = []
-for i in range(100):
-    point = (random.randint(1,50),random.randint(1,50))
-    vList.append(point)
+vlist = []
+for i in range(1000):
+    point = (random.randint(1, 50), random.randint(1, 50))
+    vlist.append(point)
 
-minimum_index,  minimum_Distance, minimize_max_distance_index, minimize_max_distance, iterations = brute_force(vList, False)
+minimum_index,  minimum_Distance, minimize_max_distance_index, minimize_max_distance, iterations = brute_force(vlist, False, True, True)
+display(vlist, minimum_index,  minimum_Distance, minimize_max_distance_index, minimize_max_distance, iterations)
 
-print("-------------------")
-print("min distance:", minimum_index, "\tvalue:", minimum_Distance)
-print("minimize max distance:", minimize_max_distance_index, "\tvalue:", minimize_max_distance)
-print("-------------------")
-print("complexity O(log n**2)")
-print("numerical complexity:")
-print("n=", len(vList))
-print("iterations=", iterations)
-print("calculation n**2=", (len(vList))**2)
-print("actual  n*(n-1)=", len(vList)*(len(vList)-1))
+print("#### 5)i)Which minimization is more suitable for this application, average or maximum distance and why? ")
+print("maximum distance is more suitable since the best location for the post office is the one were it "
+      "minimizes distance from the farthest village")
+print("#### 5)ii)And which is faster to execute?")
+print("since both have the same complexity maximum distance will be faster since it "
+      "does not calculate and average before choosing the minimum distance")
+
+# Generate 1000 random points
+vlist = []
+for i in range(1000):
+    point = (random.randint(1, 50), random.randint(1, 50))
+    vlist.append(point)
+print("time for minAverage over 100000 ")
+start = time.clock()
+brute_force(vlist, False, True, False)
+print(time.clock() - start)
+
+print("time for minimizeMaximum over 100000 ")
+start = time.clock()
+brute_force(vlist, False, False, True)
+print(time.clock() - start)
 
--- README.md
+++ README.md
@@ -1,57 +1,45 @@
-C:\Users\hfuad\PycharmProjects\FormalMethodsAssignment1\venv\Scripts\python.exe C:/Users/hfuad/PycharmProjects/FormalMethodsAssignment1/Assignment1.py
+# Formal Methods of Computer Algorithms - Assignment 1
+	Riham Nour  182031
+	Hussam Fuad 161101
+## Sample points
 [(20, 20), (30, 50), (60, 70), (20, 30), (90, 5)]
-	 0 : 1 	 31.622776601683793
-	 0 : 2 	 64.03124237432849
-	 0 : 3 	 10.0
-	 0 : 4 	 71.58910531638176
-avg: 0 : 44.31078107309851
+### display for n= 5
+#### 1) Using brute-force approach, design an algorithm to find the post-office location minimizing the average distance between the villages and the post office.
+	min distance: 3 	value: 40.81489150162855
 
-	 1 : 0 	 31.622776601683793
-	 1 : 2 	 36.05551275463989
-	 1 : 3 	 22.360679774997898
-	 1 : 4 	 75.0
-avg: 1 : 41.259742282830395
+#### 2) Using brute-force approach, design an algorithm to find the post-office location minimizing the maximum distance from a village to the post office.
+	minimize max: 0 	value: 71.58910531638176
 
-	 2 : 0 	 64.03124237432849
-	 2 : 1 	 36.05551275463989
-	 2 : 3 	 56.568542494923804
-	 2 : 4 	 71.58910531638176
-avg: 2 : 57.061100735068486
-
-	 3 : 0 	 10.0
-	 3 : 1 	 22.360679774997898
-	 3 : 2 	 56.568542494923804
-	 3 : 4 	 74.33034373659252
-avg: 3 : 40.81489150162855
-
-	 4 : 0 	 71.58910531638176
-	 4 : 1 	 75.0
-	 4 : 2 	 71.58910531638176
-	 4 : 3 	 74.33034373659252
-avg: 4 : 73.12713859233901
+#### 4) Analyze your designed algorithms mathematically and empirically.
+	Empirical Analysis : complexity O(log n**2)
+	numerical Analysis : 
+		n= 5
+		iterations= 20
+		calculation n**2= 25
+		actual  n*(n-1)= 20
 
 -------------------
-min distance: 3 	value: 40.81489150162855
-minimize max distance: 0 	value: 71.58910531638176
--------------------
-complexity O(log n**2)
-numerical complexity:
-n= 5
-iterations= 20
-calculation n**2= 25
-actual  n*(n-1)= 20
-######################
-#rerun for 100 random points
-######################
--------------------
-min distance: 47 	value: 19.266255609647207
-minimize max distance: 47 	value: 32.55764119219941
--------------------
-complexity O(log n**2)
-numerical complexity:
-n= 100
-iterations= 9900
-calculation n**2= 10000
-actual  n*(n-1)= 9900
+### display for n= 1000
+#### 1) Using brute-force approach, design an algorithm to find the post-office location minimizing the average distance between the villages and the post office.
+	min distance: 33 	value: 18.809185851542445
 
-Process finished with exit code 0
+#### 2) Using brute-force approach, design an algorithm to find the post-office location minimizing the maximum distance from a village to the post office.
+	minimize max: 33 	value: 33.97057550292606
+
+#### 4) Analyze your designed algorithms mathematically and empirically.
+	Empirical Analysis : complexity O(log n**2)
+	numerical Analysis : 
+		n= 1000
+		iterations= 999000
+		calculation n**2= 1000000
+		actual  n*(n-1)= 999000
+
+-------------------
+#### 5)i)Which minimization is more suitable for this application, average or maximum distance and why? 
+maximum distance is more suitable since the best location for the post office is the one were it minimizes distance from the farthest village
+#### 5)ii)And which is faster to execute?
+since both have the same complexity maximum distance will be faster since it does not calculate and average before choosing the minimum distance
+time for minAverage over 100000 
+1.3433499989999997
+time for minimizeMaximum over 100000 
+1.248110418
